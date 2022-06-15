f8a5e44222827835f231775c45c2beeef441f91c
tiagosantooz <tiago_slash@hotmail.com>
2022-06-15 16:36:29 +0100

    Correção das tabelas de acordo com o modelo ER reformulado.
    Tabela e modelo : Dieta

--- app/src/main/java/com/example/projetoprogramacaoavancada/TabelaDBdieta.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/TabelaDBdieta.kt
@@ -5,14 +5,14 @@ import android.provider.BaseColumns
 
 class TabelaDBdieta(db: SQLiteDatabase) : TabelaDb(db, NOME){
      override fun cria() {
-        db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT , $CAMPO_NOME TEXT NOT NULL , $CAMPO_DESCRICAO TEXT NOT NULL , FOREIGN KEY ($CAMPO_ALIMENTO_ID) REFERENCES ${TabelaDBalimento.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT)")
+        db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT , $CAMPO_NOME TEXT NOT NULL , $CAMPO_DESCRICAO TEXT NOT NULL , FOREIGN KEY ($CAMPO_UTILIZADOR_ID) REFERENCES ${TabelaDButilizador.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT)")
     }
 
     companion object {
         const val NOME = "pa_exercicio"
         const val CAMPO_NOME = "nome"
         const val CAMPO_DESCRICAO = "descricao"
-        const val CAMPO_ALIMENTO_ID = "alimentoId"
+        const val CAMPO_UTILIZADOR_ID = "utilizadorId"
     }
 }
 

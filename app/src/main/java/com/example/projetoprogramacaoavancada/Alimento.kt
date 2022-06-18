050b451b39d192d9edd73d50691a531a220c2f9d
tiagosantooz <tiago_slash@hotmail.com>
2022-06-18 15:08:14 +0100

    Criado função para converter um cursor num alimento.

--- app/src/main/java/com/example/projetoprogramacaoavancada/Alimento.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/Alimento.kt
@@ -1,6 +1,9 @@
 package com.example.projetoprogramacaoavancada
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
+
 
 data class Alimento(
     var nome : String,
@@ -16,5 +19,22 @@ data class Alimento(
         valores.put(TabelaDBalimento.CAMPO_QUANTIDADE_ALIMENTO, quantidade)
         valores.put(TabelaDBalimento.CAMPO_CALORIA_ALIMENTO, calorias)
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor): Alimento{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaDBalimento.CAMPO_NOME_ALIMENTO)
+            val posQuant = cursor.getColumnIndex(TabelaDBalimento.CAMPO_QUANTIDADE_ALIMENTO)
+            val posCal = cursor.getColumnIndex(TabelaDBalimento.CAMPO_CALORIA_ALIMENTO)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+            val quantidade = cursor.getInt(posQuant)
+            val calorias = cursor.getInt(posCal)
+
+            return Alimento(nome, quantidade,calorias,id)
+
+        }
     }
 }

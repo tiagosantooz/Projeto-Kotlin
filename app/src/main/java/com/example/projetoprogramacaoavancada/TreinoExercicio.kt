f7cb976b0765ee72935e1204d3eadb44d3b873ef
tiagosantooz <tiago_slash@hotmail.com>
2022-06-18 16:01:13 +0100

    Criado função para converter um cursor num TreinoExercicio.

--- app/src/main/java/com/example/projetoprogramacaoavancada/TreinoExercicio.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/TreinoExercicio.kt
@@ -1,6 +1,8 @@
 package com.example.projetoprogramacaoavancada
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class TreinoExercicio (
     var idexercicio : Long,
@@ -14,6 +16,20 @@ data class TreinoExercicio (
         valores.put(TabelaDBtreinoExercicio.CAMPO_ID_TREINO, idtreino)
 
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor): TreinoExercicio{
+
+            val posIdEx = cursor.getColumnIndex(TabelaDBtreinoExercicio.CAMPO_ID_EXERCICIO)
+            val posIdTrei = cursor.getColumnIndex(TabelaDBtreinoExercicio.CAMPO_ID_TREINO)
+
+            val idexercicio = cursor.getLong(posIdEx)
+            val idtreino = cursor.getLong(posIdTrei)
+
+            return TreinoExercicio(idexercicio,idtreino)
+
+        }
     }
 }
 

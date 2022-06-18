b1ab3963ccaca65cc9a5990e62e30e9794d3b95e
tiagosantooz <tiago_slash@hotmail.com>
2022-06-18 15:21:51 +0100

    Criado função para converter um cursor num Exercicio.

--- app/src/main/java/com/example/projetoprogramacaoavancada/Exercicio.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/Exercicio.kt
@@ -1,6 +1,8 @@
 package com.example.projetoprogramacaoavancada
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Exercicio(
 
@@ -25,6 +27,26 @@ data class Exercicio(
 
 
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor): Exercicio{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaDBexercicio.CAMPO_NOME)
+            val posDesc = cursor.getColumnIndex(TabelaDBexercicio.CAMPO_DESCRICAO)
+            val posMaq = cursor.getColumnIndex(TabelaDBexercicio.CAMPO_MAQUINA)
+            val posCarga = cursor.getColumnIndex(TabelaDBexercicio.CAMPO_CARGA)
+            val posRep = cursor.getColumnIndex(TabelaDBexercicio.CAMPO_REPETICOES)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+            val descricao = cursor.getString(posDesc)
+            val maquina = cursor.getLong(posMaq)
+            val carga = cursor.getLong(posCarga)
+            val repeticoes = cursor.getLong(posRep)
+
+            return Exercicio(nome, descricao, maquina, carga, repeticoes, id)
+        }
     }
 }
 

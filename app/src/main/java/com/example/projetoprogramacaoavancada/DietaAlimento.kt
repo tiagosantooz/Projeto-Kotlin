79ab6b2b0b5d49ed076dad8875e6d687f7b89d65
tiagosantooz <tiago_slash@hotmail.com>
2022-06-18 15:12:46 +0100

    Criado função para converter um cursor numa DietaAlimento.

--- app/src/main/java/com/example/projetoprogramacaoavancada/DietaAlimento.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/DietaAlimento.kt
@@ -1,6 +1,7 @@
 package com.example.projetoprogramacaoavancada
 
 import android.content.ContentValues
+import android.database.Cursor
 
 data class DietaAlimento (
     var idalimento : Long,
@@ -14,6 +15,19 @@ data class DietaAlimento (
         valores.put(TabelaDBdietaAlimento.CAMPO_ID_DIETA, iddieta)
 
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor): DietaAlimento{
+            val posIdAli = cursor.getColumnIndex(TabelaDBdietaAlimento.CAMPO_ID_ALIMENTO)
+            val posIdDieta = cursor.getColumnIndex(TabelaDBdietaAlimento.CAMPO_ID_DIETA)
+
+            val idalimento = cursor.getLong(posIdAli)
+            val iddieta = cursor.getLong(posIdDieta)
+
+            return DietaAlimento(idalimento,iddieta)
+
+        }
     }
 }
 

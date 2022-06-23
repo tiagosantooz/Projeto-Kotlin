4423d6ca036a9183edad138a671ea00ed2132d35
tiagosantooz <tiago_slash@hotmail.com>
2022-06-23 18:51:01 +0100

    Implementada a função update do ContentProviderGym.

--- app/src/main/java/com/example/projetoprogramacaoavancada/ContentProviderGym.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/ContentProviderGym.kt
@@ -118,7 +118,25 @@ class ContentProviderGym : ContentProvider() {
         selection: String?,
         selectionArgs: Array<out String>?
     ): Int {
-        TODO("Not yet implemented")
+        requireNotNull(values)
+
+        val db = dbOpenHelper!!.writableDatabase
+
+        val id = uri.lastPathSegment
+
+        val registosAlterados = when (getUriMatcher().match(uri)){
+            URI_UTILIZADOR_ESPECIFICO -> TabelaDButilizador(db).update(values, "${BaseColumns._ID}=?", arrayOf("$id"))
+            URI_DIETA_ESPECIFA -> TabelaDBdieta(db).update(values, "${BaseColumns._ID}=?", arrayOf("$id"))
+            URI_TREINO_ESPECIFICO -> TabelaDBtreino(db).update(values, "${BaseColumns._ID}=?", arrayOf("$id"))
+            URI_ALIMENTO_ESPECIFICO -> TabelaDBalimento(db).update(values, "${BaseColumns._ID}=?", arrayOf("$id"))
+            URI_EXERCICIOS_ESPECIFICO -> TabelaDBexercicio(db).update(values, "${BaseColumns._ID}=?", arrayOf("$id"))
+
+            else -> 0
+        }
+
+        db.close()
+
+        return registosAlterados
     }
 
     companion object{

e9638f8c5808a7aefd19dd4649be9a2b2cb11270
tiagosantooz <tiago_slash@hotmail.com>
2022-07-16 17:29:22 +0100

    Implementar a função onBindViewHolder no AdapterUtilizador.

--- app/src/main/java/com/example/projetoprogramacaoavancada/AdapterUtilizador.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/AdapterUtilizador.kt
@@ -3,6 +3,7 @@ package com.example.projetoprogramacaoavancada
 import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class AdapterUtilizador(val fragment: ListaUtilizadorFragment) : RecyclerView.Adapter<AdapterUtilizador.ViewHolderUtilizador>() {
@@ -15,17 +16,35 @@ class AdapterUtilizador(val fragment: ListaUtilizadorFragment) : RecyclerView.Ad
             }
         }
 
-    class ViewHolderUtilizador(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolderUtilizador(itemUtilizador: View) : RecyclerView.ViewHolder(itemUtilizador) {
+        val textName = itemUtilizador.findViewById<TextView>(R.id.textName)
+        val textGender = itemUtilizador.findViewById<TextView>(R.id.textGender)
+        val textAge = itemUtilizador.findViewById<TextView>(R.id.textAge)
+        val textHeight = itemUtilizador.findViewById<TextView>(R.id.textHeight)
+        val textWeight = itemUtilizador.findViewById<TextView>(R.id.textWeight)
 
+        var utilizador : Utilizador? =  null
+            get()=field
+            set(value: Utilizador?) {
+                field=value
+
+                textName.text=utilizador?.nome?:""
+                textGender.text=utilizador?.sexo?:""
+                textAge.text=(utilizador?.idade?:"").toString()
+                textHeight.text=(utilizador?.altura?:"").toString()
+                textWeight.text=(utilizador?.peso?:"").toString()
+
+            }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderUtilizador {
-        val itemUtilizador = fragment.layoutInflater.inflate(R.layout.item_utilzador, parent, false)
+        val itemUtilizador = fragment.layoutInflater.inflate(R.layout.item_utilizador, parent, false)
         return ViewHolderUtilizador(itemUtilizador)
     }
 
     override fun onBindViewHolder(holder: ViewHolderUtilizador, position: Int) {
-        TODO("Not yet implemented")
+        cursor!!.moveToPosition(position)
+        holder.utilizador=Utilizador.fromCursor(cursor!!)
     }
 
     override fun getItemCount(): Int {

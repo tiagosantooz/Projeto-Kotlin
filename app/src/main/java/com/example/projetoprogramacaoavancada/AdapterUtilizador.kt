d1171802e32e0a3ab60311bd3c88086d2b2334f5
tiagosantooz <tiago_slash@hotmail.com>
2022-07-16 16:41:39 +0100

    Criado o AdapterUtilizador para comunicar com a ReciclerView.

---
+++ app/src/main/java/com/example/projetoprogramacaoavancada/AdapterUtilizador.kt
@@ -0,0 +1,24 @@
+package com.example.projetoprogramacaoavancada
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterUtilizador : RecyclerView.Adapter<AdapterUtilizador.ViewHolderUtilizador>() {
+    class ViewHolderUtilizador(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderUtilizador {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderUtilizador, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}

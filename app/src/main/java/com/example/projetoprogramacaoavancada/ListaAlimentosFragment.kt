6adcad51e958209597c671ad291405fd2396ce8b
tiagosantooz <tiago_slash@hotmail.com>
2022-07-17 20:19:52 +0100

    Small fix.

--- app/src/main/java/com/example/projetoprogramacaoavancada/ListaAlimentosFragment.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/ListaAlimentosFragment.kt
@@ -38,8 +38,8 @@ class ListaAlimentosFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor>
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_alimentos, container, false)
+        _binding = FragmentListaAlimentosBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {

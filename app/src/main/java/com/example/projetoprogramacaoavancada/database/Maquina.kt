9acb428696644a44499b1de60b2888b4905d53ec
tiagosantooz <tiago_slash@hotmail.com>
2022-07-22 20:17:47 +0100

    Conclusão da tabela maquina.

--- app/src/main/java/com/example/projetoprogramacaoavancada/database/Maquina.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/database/Maquina.kt
@@ -1,4 +1,28 @@
 package com.example.projetoprogramacaoavancada.database
 
-class Maquina {
+import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
+import java.io.Serializable
+
+data class Maquina(var nome : String ="", var id: Long = -1) : Serializable {
+    fun toContentValues() : ContentValues{
+        val valores = ContentValues()
+        valores.put(TabelaDBmaquina.CAMPO_NOME, nome)
+
+        return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor): Maquina{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaDBmaquina.CAMPO_NOME)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+
+            return Maquina(nome, id)
+
+        }
+    }
 }
--- app/src/main/java/com/example/projetoprogramacaoavancada/database/TabelaDBmaquina.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/database/TabelaDBmaquina.kt
@@ -1,4 +1,22 @@
 package com.example.projetoprogramacaoavancada.database
 
-class TabelaDBmaquina {
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+
+class TabelaDBmaquina (db : SQLiteDatabase) : TabelaDb(db, NOME) {
+    override fun cria() {
+        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL)")
+
+    }
+
+    companion object{
+        const val NOME = "maquinas"
+
+        const val CAMPO_ID = "$NOME.${BaseColumns._ID}"
+        const val CAMPO_NOME = "nome"
+
+        val TODAS_COLUNAS = arrayOf(CAMPO_ID, CAMPO_NOME)
+    }
 }
+

5730d7974289dc33ecb520cef8eefb03682a5500
tiagosantooz <tiago_slash@hotmail.com>
2022-07-23 14:57:24 +0100

    Adicionado binding para aceder às variáveis do fragmento e criação do layout de inserir exercicio.

--- app/src/main/java/com/example/projetoprogramacaoavancada/InserirExercicioFragment.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/InserirExercicioFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.projetoprogramacaoavancada.databinding.FragmentInserirExercicioBinding
 
 
 class InserirExercicioFragment : Fragment() {
+    private var _binding : FragmentInserirExercicioBinding? =null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inserir_exercicio, container, false)
+        _binding = FragmentInserirExercicioBinding.inflate(inflater,container,false)
+        return inflater.inflate(R.layout.fragment_inserir_exercicio,container,false)
+
     }
 
 

8d1849af26efb7e96fffae61dd703cc25619b1a2
tiagosantooz <tiago_slash@hotmail.com>
2022-07-23 14:02:16 +0100

    Criado um Adptador de Exercicios para comunicar com o recycler view.

---
+++ app/src/main/java/com/example/projetoprogramacaoavancada/database/AdapterExercicios.kt
@@ -0,0 +1,25 @@
+package com.example.projetoprogramacaoavancada.database
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterExercicios : RecyclerView.Adapter<AdapterExercicios.ViewHolderExercicio>() {
+
+    class ViewHolderExercicio(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderExercicio {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderExercicio, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}

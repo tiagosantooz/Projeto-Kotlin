38faa76cccc85b0455bf98e8df5af9b01a59ad7c
tiagosantooz <tiago_slash@hotmail.com>
2022-07-23 13:40:13 +0100

    Implementado a interface LoaderManager.LoaderCallbacks<Cursor> no fragment lista de exercicios.

--- app/src/main/java/com/example/projetoprogramacaoavancada/ListaExerciciosFragment.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/ListaExerciciosFragment.kt
@@ -1,13 +1,16 @@
 package com.example.projetoprogramacaoavancada
 
+import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.Loader
 
 
-class ListaExerciciosFragment : Fragment() {
+class ListaExerciciosFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +24,17 @@ class ListaExerciciosFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lista_exercicios, container, false)
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
     }
 }

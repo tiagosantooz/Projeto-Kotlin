5c9f3ab5240353c0c906d821fa9868b780d63161
tiagosantooz <tiago_slash@hotmail.com>
2022-07-23 14:22:43 +0100

    Implementada a função onCreateViewHolder no Adapter de Exercicios e mostrar o exercicio no viewholder.

--- app/src/main/java/com/example/projetoprogramacaoavancada/database/AdapterExercicios.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/database/AdapterExercicios.kt
@@ -3,6 +3,7 @@ package com.example.projetoprogramacaoavancada.database
 import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projetoprogramacaoavancada.ListaExerciciosFragment
 import com.example.projetoprogramacaoavancada.R
@@ -18,8 +19,24 @@ class AdapterExercicios (val fragment: ListaExerciciosFragment): RecyclerView.Ad
         }
 
 
-    class ViewHolderExercicio(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolderExercicio(itemExercicio: View) : RecyclerView.ViewHolder(itemExercicio){
+        val textViewNome = itemExercicio.findViewById<TextView>(R.id.textViewNomeEx)
+        val textViewDesc = itemExercicio.findViewById<TextView>(R.id.textViewDescEx)
+        val textViewCarga = itemExercicio.findViewById<TextView>(R.id.textViewCargEx)
+        val textViewRep = itemExercicio.findViewById<TextView>(R.id.textViewRepEx)
+        val textViewMaquina = itemExercicio.findViewById<TextView>(R.id.textViewNomeMaqEx)
 
+        var exercicio : Exercicio? = null
+            get() = field
+            set(value : Exercicio?){
+                field = value
+
+                textViewNome.text = exercicio?.nome ?:""
+                textViewDesc.text = exercicio?.descricao ?:""
+                textViewCarga.text = (exercicio?.carga ?:"").toString()
+                textViewRep.text = (exercicio?.repeticoes ?:"").toString()
+                textViewMaquina.text = exercicio?.maquina?.nome ?:""
+            }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderExercicio {
@@ -28,7 +45,8 @@ class AdapterExercicios (val fragment: ListaExerciciosFragment): RecyclerView.Ad
     }
 
     override fun onBindViewHolder(holder: ViewHolderExercicio, position: Int) {
-        TODO("Not yet implemented")
+   cursor!!.moveToPosition(position)
+        holder.exercicio = Exercicio.fromCursor(cursor!!)
     }
 
     override fun getItemCount(): Int {

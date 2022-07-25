37d67bcf6d0e0355a6033ce3e0747896375f0ee8
tiagosantooz <tiago_slash@hotmail.com>
2022-07-25 16:34:21 +0100

    bUG FIX.

--- app/src/main/java/com/example/projetoprogramacaoavancada/database/TabelaDBtreino.kt
+++ app/src/main/java/com/example/projetoprogramacaoavancada/database/TabelaDBtreino.kt
@@ -7,10 +7,11 @@ import android.provider.BaseColumns
 
 class TabelaDBtreino(db: SQLiteDatabase) : TabelaDb(db, NOME){
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT , " +
+        val  sql = "CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT , " +
                 "$CAMPO_DESCRICAO TEXT NOT NULL , " +
                 "$CAMPO_UTILIZADOR_ID INTEGER NOT NULL , " +
-                "FOREIGN KEY ($CAMPO_UTILIZADOR_ID) REFERENCES ${TabelaDButilizador.NOME}(${TabelaDButilizador.CAMPO_ID}) ON DELETE RESTRICT)")
+                "FOREIGN KEY ($CAMPO_UTILIZADOR_ID) REFERENCES ${TabelaDButilizador.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT)"
+        db.execSQL(sql)
     }
     override fun query(
         columns: Array<String>,
@@ -27,7 +28,7 @@ class TabelaDBtreino(db: SQLiteDatabase) : TabelaDb(db, NOME){
     }
     companion object {
         const val NOME = "pa_treino"
-        const val CAMPO_ID = "${TabelaDBtreino.NOME}.${BaseColumns._ID}"
+        const val CAMPO_ID = "$NOME.${BaseColumns._ID}"
         const val CAMPO_DESCRICAO = "descricao"
         const val CAMPO_UTILIZADOR_ID = "utilizadorid"
 
